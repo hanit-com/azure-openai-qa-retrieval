609cfe8061f15cc33d77aa7f90029e67a526da56
Hanit <37485638+hanit-com@users.noreply.github.com>
2023-05-18 23:20:23 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,68 @@
+# Azure OpenAI Embedding
+
+This repository utilizes Azure OpenAI Embedding service to answer questions and interact with your own documented data. 
+
+## Prerequisites
+You should have an Azure account and a running instance of OpenAI.
+
+## Installation
+
+1. Clone the repository:
+
+   ```shell
+   git clone https://github.com/your-username/your-project.git
+   ```
+
+2. Create a Python environment:
+
+   ```shell
+   python -m venv .env
+   ```
+
+3. Activate the environment:
+
+   ```shell
+   source .env/bin/activate
+   ```
+
+4. Install the dependencies:
+
+   ```shell
+   pip install -r requirements.txt
+   ```
+
+## Configuration
+
+1. Inside 'constants.py', complete the API key and the Azure resource endpoint base.
+<br>
+Select an instance on the [Cognitive Services Hub](https://portal.azure.com/#view/Microsoft_Azure_ProjectOxford/CognitiveServicesHub/~/OpenAI) and go to "Keys and Endpoints".
+<br>
+Use either "KEY 1" or "KEY 2" and the "Endpoint" field.
+
+   ```python
+   API_KEY = ""
+   RESOURCE_ENDPOINT = ""
+   ```
+2. Add your data files to the 'context_data/data' directory.
+
+
+## Usage
+
+Run:
+
+```shell
+python azure_openai.py
+```
+
+Prompt example:
+<br> 
+(Using data from the Wikipedia page [Coronation of Charles III and Camilla](https://en.wikipedia.org/wiki/Coronation_of_Charles_III_and_Camilla)).
+```shell
+Prompt: Where tiaras allowed at the ceremony?
+Completion: 
+No, tiaras were not allowed at the ceremony.
+Prompt: What was the venue for the coronation?
+Completion: 
+The venue for the coronation was Westminster Abbey.
+```
+
---
+++ azure_openai.py
@@ -0,0 +1,79 @@
+import os
+import openai
+from constants import API_KEY, RESOURCE_ENDPOINT
+from llama_index import LangchainEmbedding, GPTVectorStoreIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, ServiceContext, StorageContext
+from llama_index import load_index_from_storage
+from langchain.llms import AzureOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+
+
+def setup_environment():
+  os.environ["OPENAI_API_KEY"] = API_KEY
+  os.environ["OPENAI_API_BASE"] = RESOURCE_ENDPOINT
+
+  openai.api_type = "azure"
+  openai.api_version = "2022-12-01"
+  openai.api_base = os.getenv('OPENAI_API_BASE')
+  openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def get_index():
+  deployment_name = "text-davinci-003"
+  data_path = "context_data/data"
+  storage_path = "./storage"
+
+  llm = AzureOpenAI(deployment_name=deployment_name)
+  llm_predictor = LLMPredictor(llm=llm)
+  embedding_llm = LangchainEmbedding(OpenAIEmbeddings(), embed_batch_size=1)
+
+  max_input_size = 3000
+  num_output = 256
+  chunk_size_limit = 1000
+  max_chunk_overlap = 20
+  prompt_helper = PromptHelper(max_input_size=max_input_size, num_output=num_output, max_chunk_overlap=max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+  service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper, embed_model=embedding_llm)
+
+  if os.path.exists(storage_path) and os.listdir(storage_path):
+    print("loading index from storage")
+    storage_context = StorageContext.from_defaults(persist_dir=storage_path)
+    index = load_index_from_storage(storage_context, service_context=service_context)
+  else:
+    print("creating new index...")
+    documents = SimpleDirectoryReader(data_path).load_data()
+    index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
+    index.storage_context.persist()
+    print("index created")
+
+  return index
+
+
+def askGPT(index):
+
+  template = "Shortly answer the following question, do not elaborate. {}"
+  #template = "{}"
+
+  while True:
+      prompt = input("Prompt: ")
+      if prompt == "exit":
+        break
+
+      query = template.format(prompt)
+      query_engine = index.as_query_engine()
+
+      response = query_engine.query(query)
+      print(f"Completion: {response.response}")
+
+
+"""
+Questions
+
+Where tiaras allowed at the ceremony?
+What was the venue for the coronation?
+What transportation did Charles and Camilla use to travel to the abbey?
+Did the prince of Wales participate in the service?
+
+"""
+
+setup_environment()
+index = get_index()
+askGPT(index)
